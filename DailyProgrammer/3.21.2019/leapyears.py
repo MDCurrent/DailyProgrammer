ff9a96bf29bbf11f8be04d0e956fc08481c6c8c7
Matthew Current <currentmatthew@gmail.com>
2019-03-21 18:51:51 -0400

    refactored leap year calculation for future features

--- DailyProgrammer/3.21.2019/leapyears.py
+++ DailyProgrammer/3.21.2019/leapyears.py
@@ -1,27 +1,31 @@
+
+def isLeapyear(n):
+    '''Params: Integer N, Returns if N is a leap year based on revised Julian calendar'''
+    if n % 4 == 0 and n % 100 != 0 or n % 4 == 0 and n % 900 in (200, 600):
+        return True
+    else:
+        return False
+
+
 def leaps(start, end):
     count = 0
     for i in range(start, end):
-        hundredRemainder = i % 100
-        if i % 4 == 0:
-            if i % 100 == 0:
-                if i % 900 == 200 or i % 900 == 600:
-                    count += 1
-            else:
-                count += 1
+        if isLeapyear(i):
+            count += 1
     return count
 
 
 def main():
     print leaps(2, 1000)
     print leaps(1, 100)
-    print "Expected: 1 - Actual: {}".format(leaps(2016, 2017)) # 1
-    print "Expected: 0 - Actual: {}".format(leaps(2019, 2020))#  0
-    print "Expected: 0 - Actual: {}".format(leaps(1900, 1901)) # 0
-    print "Expected: 1 - Actual: {}".format(leaps(2000, 2001)) # 1
-    print "Expected: 0 - Actual: {}".format(leaps(2800, 2801)) # 0
-    print "Expected: 1 - Actual: {}".format(leaps(123456, 123456)) # 0
-    print "Expected: 1077 - Actual: {}".format(leaps(1234, 5678)) # 1077
-    print "Expected: 1881475 - Actual: {}".format(leaps(123456, 7891011)) # 1881475
+    print "Expected: 1 - Actual: {}".format(leaps(2016, 2017))  # 1
+    print "Expected: 0 - Actual: {}".format(leaps(2019, 2020))  # 0
+    print "Expected: 0 - Actual: {}".format(leaps(1900, 1901))  # 0
+    print "Expected: 1 - Actual: {}".format(leaps(2000, 2001))  # 1
+    print "Expected: 0 - Actual: {}".format(leaps(2800, 2801))  # 0
+    print "Expected: 1 - Actual: {}".format(leaps(123456, 123456))  # 0
+    print "Expected: 1077 - Actual: {}".format(leaps(1234, 5678))  # 1077
+    print "Expected: 1881475 - Actual: {}".format(leaps(123456, 7891011))  # 1881475
 
 
 if __name__ == "__main__":
